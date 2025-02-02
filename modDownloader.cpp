2f744e052a12881b6fba731ac05791bdeb2955f3
Fengying <48760372+zxcvbnm3057@users.noreply.github.com>
2025-02-03 00:01:35 +0800

    fix: 更新功能可用

--- modDownloader.cpp
+++ modDownloader.cpp
@@ -47,7 +47,7 @@ const static char* SWITCH = "切换变体";
 const static char* UPDATE = "更新";
 const static char* UNINSTALL = "卸载";
 
-int check_local_status(git_repository* repo, const char* branch);
+int check_local_status(git_repository* repo, const char** branch);
 bool list_remote_branches(std::vector<std::string>* variant_list);
 void switch_branch(git_repository* repo, std::string branch_name);
 void uninstall();
@@ -86,7 +86,7 @@ int main()
 		}
 
 		const char* branch = NULL;
-		int status = check_local_status(repo, branch);
+		int status = check_local_status(repo, &branch);
 
 		int action_code;
 		printf("请选择操作：\n");
@@ -136,7 +136,7 @@ int main()
 	system("pause");
 }
 
-static int check_local_status(git_repository* repo, const char* branch)
+static int check_local_status(git_repository* repo, const char** branch)
 {
 	int head_read_error, upstream_read_error, common_base_read_error, commit_read_error;
 	git_reference* head = NULL;
@@ -152,37 +152,44 @@ static int check_local_status(git_repository* repo, const char* branch)
 		std::cout << "未知状态" << std::endl;
 	else {
 		status = INSTALLED;
-		branch = git_reference_shorthand(head);
-		std::cout << branch;
+		*branch = git_reference_shorthand(head);
+		std::cout << *branch;
 		git_reference* local_branch = nullptr;
-		git_branch_lookup(&local_branch, repo, branch, GIT_BRANCH_LOCAL);
+		git_branch_lookup(&local_branch, repo, *branch, GIT_BRANCH_LOCAL);
 		git_reference* upstream = nullptr;
 		upstream_read_error = git_branch_upstream(&upstream, head);
 		if (!upstream_read_error)
 		{
-			git_oid merge_base_oid;
-			common_base_read_error = git_merge_base(&merge_base_oid, repo, git_reference_target(head), git_reference_target(upstream));
-			if (!common_base_read_error)
+			git_remote* remote = nullptr;
+			git_remote_lookup(&remote, repo, "origin");
+			git_reference_name(upstream);
+			char* refspec = { const_cast<char*>(git_reference_name(upstream)) };
+			git_strarray upstream_array = {
+				&refspec,
+				1
+			};
+			git_fetch_options fetch_opts = GIT_FETCH_OPTIONS_INIT;
+			fetch_opts.depth = 1;
+			fetch_opts.prune = GIT_FETCH_PRUNE;
+
+			if (git_remote_fetch(remote, &upstream_array, &fetch_opts, nullptr) == GIT_OK)
 			{
-				git_commit* local_commit = nullptr;
-				commit_read_error = git_commit_lookup(&local_commit, repo, git_reference_target(head));
-				if (!commit_read_error)
-					if (git_oid_cmp(&merge_base_oid, git_commit_id(local_commit)) != 0) {
-						std::cout << "（已过时）" << std::endl;
-						status = OUTDATE;
-					}
-					else {
-						std::cout << "（已是最新版本）" << std::endl;
-						status = OK;
-					}
-				git_commit_free(local_commit);
-				goto exit;
+				if (git_oid_cmp(git_reference_target(head), git_reference_target(upstream)) != 0) {
+					std::cout << "（已过时）" << std::endl;
+					status = OUTDATE;
+				}
+				else {
+					std::cout << "（已是最新版本）" << std::endl;
+					status = OK;
+				}
 			}
+			else
+				std::cout << "（检查更新失败）" << std::endl;
 		}
-		std::cout << "（检查更新失败）" << std::endl;
-	exit:
-		return status;
+		else
+			std::cout << "（检查更新失败）" << std::endl;
 	}
+	return status;
 }
 
 static bool list_remote_branches(std::vector<std::string>* variant_list)
@@ -209,6 +216,7 @@ static bool list_remote_branches(std::vector<std::string>* variant_list)
 }
 
 static void switch_branch(git_repository* repo, std::string branch_name) {
+	printf("正在处理...\n");
 	git_remote* remote = nullptr;
 	git_remote_lookup(&remote, repo, "origin");
 	std::string refspec_str = "refs/heads/" + branch_name;
